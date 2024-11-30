c41e4691e01f9e08c50e47fbc9890cf0540b8804
Nugget-Meister <tremainecamacho@gmail.com>
2024-11-30 18:15:27 -0500

    redirect 'should' work now

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import {Route, BrowserRouter as Router, Routes} from "react-router-dom"
 import './App.css'
 import Home from './components/Home/Home'
 import SignIn from './components/SignIn/SignIn'
+import Redirect from './components/Redirect/Redirect'
 
 function App() {
 
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path={'/'} element={<Home/>} />
         <Route path={'/sign-in'} element={<SignIn/>}/>
+        <Route path={'/*'} element={<Redirect/>}/>
       </Routes>
     </Router>
 
