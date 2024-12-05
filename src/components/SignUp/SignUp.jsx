cfe2329fd7988e6a5d859bebda120c6257838565
Nugget-Meister <tremainecamacho@gmail.com>
2024-12-05 12:07:07 -0500

    replace public with remote img link

--- src/components/SignUp/SignUp.jsx
+++ src/components/SignUp/SignUp.jsx
@@ -6,8 +6,8 @@ import PasswordBox from '../SignUp/subcomponent/PasswordBox';
 import Container from '../subcomponents/Container';
 // import signInImg from '/public/signInImg.jpg'
 
-// const imgURL = 'https://images.unsplash.com/photo-1669251921941-ae3645715017?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
-import imgURL from '/public/signInImg.jpg'
+const imgURL = 'https://images.unsplash.com/photo-1669251921941-ae3645715017?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D'
+// import imgURL from '/public/signInImg.jpg'
 
 const SignUp = () => {
     
